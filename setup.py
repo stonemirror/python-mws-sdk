18ea6728aee7b2be94fb9b6faf31c269ade2f59e
Juozas Kaziukenas <juozas@juokaz.com>
2015-06-30 20:30:05 -0400

    Fix release

--- setup.py
+++ setup.py
@@ -1,38 +1,17 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 version = '0.2'
-
-REQUIREMENTS = ['requests']
-
-CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Topic :: Software Development',
-    'Topic :: Software Development :: Libraries :: Application Frameworks',
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.4",
-]
 
 setup(
     name="amazon-mws",
     version=version,
     description="A python interface for Amazon MWS",
+    long_description=open('README.md', 'rt').read(),
     author="Juozas Kaziukenas",
     author_email="juozas@juokaz.com",
     url="https://github.com/juokaz/python-amazon-mws",
-    packages=find_packages(),
+    packages=['mws'],
     platforms=['OS Independent'],
-    license='LICENSE.txt',
-    install_requires=REQUIREMENTS,
-    classifiers=CLASSIFIERS,
-    include_package_data=True,
-    zip_safe=False
+    license='LICENSE.txt'
 )
